d8d74e8f556b25225fef5a4ad080495d11266421
YulikK <yulikpoch@gmail.com>
2024-02-02 20:25:32 +0100

    refactor: delete init function

--- nonograms/src/app.js
+++ nonograms/src/app.js
@@ -8,14 +8,10 @@ import WinPresenter from "./js/presenter/win.js";
 const siteBodyElement = document.body;
 
 if (siteBodyElement !== null) {
+  
   const crosswordModel = new CrosswordModel();
-
-  const initData = (data) => {
-    const crosswords = data.map(crosswordModel.adaptToClient);
-    crosswordModel.setCrosswords(crosswords);
-  };
-
-  initData(mockFile);
+  const crosswords = mockFile.map(crosswordModel.adaptToClient);
+  crosswordModel.setCrosswords(crosswords);
 
   const winPresenter = new WinPresenter(siteBodyElement);
   const galleryPresenter = new GalleryPresenter(
